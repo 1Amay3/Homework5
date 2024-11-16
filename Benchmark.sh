3b7bb94ad01bfd7e12b3c854b730b16960afadf4
1Amay3 <74832417+1Amay3@users.noreply.github.com>
2024-11-16 18:41:51 -0500

    Update Benchmark.sh

--- Benchmark.sh
+++ Benchmark.sh
@@ -10,7 +10,7 @@ readPercentage=(20 40 60 80)
 for set_type in "${queues[@]}"; do
     for thread_count in "${threads[@]}"; do
         for contains_percentage in "${readPercentage[@]}"; do
-            echo "$set_type for $thread_count threads with $readPercentage% read operation"
+            echo "$queue for $thread_count threads with $readPercentage% read operation"
             contains_decimal=$(echo "scale=2; $contains_percentage" | bc)
             java -cp build/libs/homework5.jar edu.vt.ece.hw5.Benchmark "$set_type" "$thread_count" "$contains_decimal"
         done
