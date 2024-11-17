4a39ccb223725409d8dbcbf2bcd8e8e1de06eea4
1Amay3 <74832417+1Amay3@users.noreply.github.com>
2024-11-16 19:21:01 -0500

    Update OptimisticSet.java

--- src/main/java/edu/vt/ece/hw5/sets/OptimisticSet.java
+++ src/main/java/edu/vt/ece/hw5/sets/OptimisticSet.java
@@ -7,40 +7,40 @@ public class OptimisticSet<T> implements Set<T> {
     private final Node<T> head;
 
     public OptimisticSet() {
-        head = new Node<>(Integer.MIN_VALUE);
-        head.next = new Node<>(Integer.MAX_VALUE);
+        head=new Node<>(Integer.MIN_VALUE);
+        head.next=new Node<>(Integer.MAX_VALUE);
     }
 
     private boolean validate(Node<T> pred, Node<T> curr) {
-        Node<T> node = head;
-        while (node.key <= pred.key) {
-            if (node == pred)
-                return pred.next == curr;
-            node = node.next;
+        Node<T> node=head;
+        while (node.key<=pred.key) {
+            if (node==pred)
+                return pred.next==curr;
+            node=node.next;
         }
         return false;
     }
 
     @Override
     public boolean add(T item) {
-        int key = item.hashCode();
+        int key=item.hashCode();
         while (true) {
-            Node<T> pred = head;
-            Node<T> curr = pred.next;
-            while (curr.key < key) {
-                pred = curr;
-                curr = curr.next;
+            Node<T> pred=head;
+            Node<T> curr=pred.next;
+            while (curr.key<key) {
+                pred=curr;
+                curr=curr.next;
             }
             pred.lock();
             try {
                 curr.lock();
                 try {
-                    if (validate(pred, curr)) {
-                        if (curr.key == key) {
+                    if (validate(pred,curr)) {
+                        if (curr.key==key) {
                             return false;
                         } else {
-                            Node<T> node = new Node<>(item, curr);
-                            pred.next = node;
+                            Node<T> node=new Node<>(item,curr);
+                            pred.next=node;
                             return true;
                         }
                     }
@@ -55,23 +55,23 @@ public class OptimisticSet<T> implements Set<T> {
 
     @Override
     public boolean remove(T item) {
-        int key = item.hashCode();
+        int key=item.hashCode();
         while (true) {
-            Node<T> pred = head;
-            Node<T> curr = pred.next;
-            while (curr.key < key) {
-                pred = curr;
-                curr = curr.next;
+            Node<T> pred=head;
+            Node<T> curr=pred.next;
+            while (curr.key<key) {
+                pred=curr;
+                curr=curr.next;
             }
             pred.lock();
             try {
                 curr.lock();
                 try {
-                    if (validate(pred, curr)) {
-                        if (curr.key != key) {
+                    if (validate(pred,curr)) {
+                        if (curr.key!=key) {
                             return false;
                         } else {
-                            pred.next = curr.next;
+                            pred.next=curr.next;
                             return true;
                         }
                     }
@@ -86,20 +86,20 @@ public class OptimisticSet<T> implements Set<T> {
 
     @Override
     public boolean contains(T item) {
-        int key = item.hashCode();
+        int key=item.hashCode();
         while (true) {
-            Node<T> pred = head;
-            Node<T> curr = pred.next;
-            while (curr.key < key) {
-                pred = curr;
-                curr = curr.next;
+            Node<T> pred=head;
+            Node<T> curr=pred.next;
+            while (curr.key<key) {
+                pred=curr;
+                curr=curr.next;
             }
             pred.lock();
             try {
                 curr.lock();
                 try {
-                    if (validate(pred, curr)) {
-                        return (curr.key == key);
+                    if (validate(pred,curr)) {
+                        return (curr.key==key);
                     }
                 } finally {
                     curr.unlock();
@@ -111,21 +111,21 @@ public class OptimisticSet<T> implements Set<T> {
     }
 
     private static class Node<U> {
-        private final Lock lock = new ReentrantLock();
+        private final Lock lock=new ReentrantLock();
         final int key;
         volatile Node<U> next;
         final U item;
 
         public Node(U item, Node<U> next) {
-            this.item = item;
-            this.key = item.hashCode();
-            this.next = next;
+            this.item=item;
+            this.key=item.hashCode();
+            this.next=next;
         }
 
         public Node(int key) {
-            this.item = null;
-            this.key = key;
-            this.next = null;
+            this.item=null;
+            this.key=key;
+            this.next=null;
         }
 
         void lock() { lock.lock(); }
